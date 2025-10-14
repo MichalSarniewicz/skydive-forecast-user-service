e73bf7a2e66c909851c57bbb01b1ac0ac5c5d3c3
MichalSarniewicz <MichalSarniewicz@users.noreply.github.com>
2025-10-14 21:53:35 +0200

    refactor: move forecast feature to the new microservice

--- src/main/java/com/skydiveforecast/domain/model/Forecast.java
+++
@@ -1,9 +0,0 @@
-package com.skydiveforecast.domain.model;
-
-import java.util.List;
-
-public record Forecast(
-        double latitude,
-        double longitude,
-        List<WeatherPoint> hours
-) {}
--- src/main/java/com/skydiveforecast/domain/port/WeatherForecastPort.java
+++
@@ -1,9 +0,0 @@
-package com.skydiveforecast.domain.port;
-
-import com.skydiveforecast.domain.model.Forecast;
-
-import java.time.LocalDate;
-
-public interface WeatherForecastPort {
-    Forecast getHourlyForecast(double latitude, double longitude, LocalDate date);
-}
