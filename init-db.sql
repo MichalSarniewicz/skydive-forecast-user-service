109a79c369b23b1c39317c89f13af4feda476722
MichalSarniewicz <MichalSarniewicz@users.noreply.github.com>
2025-10-23 08:37:00 +0200

    fix: change the schema name to skydive_forecast_user in the init-db file

--- init-db.sql
+++ init-db.sql
@@ -1,11 +1,11 @@
 -- Create a schema for the application
-CREATE SCHEMA IF NOT EXISTS "skydive-forecast-user";
+CREATE SCHEMA IF NOT EXISTS skydive_forecast_user;
 
 -- Grant privileges to the application user
-GRANT ALL PRIVILEGES ON SCHEMA "skydive-forecast-user" TO skydive_forecast_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA "skydive-forecast-user" TO skydive_forecast_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA "skydive-forecast-user" TO skydive_forecast_user;
+GRANT ALL PRIVILEGES ON SCHEMA skydive_forecast_user TO skydive_forecast_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA skydive_forecast_user TO skydive_forecast_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA skydive_forecast_user TO skydive_forecast_user;
 
 -- Set default privileges for future tables
-ALTER DEFAULT PRIVILEGES IN SCHEMA "skydive-forecast-user" GRANT ALL PRIVILEGES ON TABLES TO skydive_forecast_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA "skydive-forecast-user" GRANT ALL PRIVILEGES ON SEQUENCES TO skydive_forecast_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA skydive_forecast_user GRANT ALL PRIVILEGES ON TABLES TO skydive_forecast_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA skydive_forecast_user GRANT ALL PRIVILEGES ON SEQUENCES TO skydive_forecast_user;
